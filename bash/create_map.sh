55883e62b692dd8c2540a9942efb0ac5c910ba28
Casey Link <unnamedrambler@gmail.com>
2011-03-14 01:44:53 -0400

    Fix relative path issue

--- bash/create_map.sh
+++ bash/create_map.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-FH_DIR=/home/ramblurr/src/fh/engine
+FH_DIR=/home/ramblurr/src/fh/engine/game
 LISTGALAXY=../bin/ListGalaxy
 STARMAP=../bin/PrintMap
 PS2PDF=/usr/bin/ps2pdf
 #############3
-
+cd $FH_DIR
 $LISTGALAXY -p | awk 'BEGIN { OFS = ", "} {print $3,$6,$9,"unnamed",$13"."}' > /tmp/fh.map.raw.$$
 num=`wc -l /tmp/fh.map.raw.$$ | awk '{print $1}'` 
 head -n $(($num-4)) /tmp/fh.map.raw.$$ > /tmp/fh.map.$$
 cd /tmp
 
 #3d map
-$STARMAP -t /tmp/fh.map.$$
+$FH_DIR/$STARMAP -t /tmp/fh.map.$$
 $PS2PDF -dAutoRotatePages=/None /tmp/fh.map.$$.ps $FH_DIR/galaxy_map_3d.pdf
 
 
-$STARMAP /tmp/fh.map.$$
+$FH_DIR/$STARMAP /tmp/fh.map.$$
 $PS2PDF -dAutoRotatePages=/None /tmp/fh.map.$$.ps $FH_DIR/galaxy_map.pdf
 
 rm /tmp/fh.map.*
