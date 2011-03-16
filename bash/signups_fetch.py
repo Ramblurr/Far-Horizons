8b8d9817451424df1a6d7dbfd070192ed34ce9f4
Casey Link <unnamedrambler@gmail.com>
2011-03-15 22:52:25 -0400

    create a csv email list

--- bash/signups_fetch.py
+++ bash/signups_fetch.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python2
-# Utility to read player registrations from a google spreadsheet
-# and save them in players.csv
-#
-# After signups are closed and all the registrations are verified (see
-# signups_verify.py) run this script to download and save the registrations
-# to a CSV file in the format:
-# email, species, home_planet, gov_name, gov_type, mil, grav, life, bio
+"""
+ Utility to read player registrations from a google spreadsheet
+ and save them in players.csv
+
+ After signups are closed and all the registrations are verified (see
+ signups_verify.py) run this script to download and save the registrations
+ to a CSV file in the format:
+     email, species, home_planet, gov_name, gov_type, mil, grav, life, bio
+
+ Additionally this script creates players_email.csv which is a simple csv in the format:
+    species name, email
+This CSV can be imported to GMail as a group, useful for sending mass messages
+to the players.
+"""
 
 import fhutils
 
@@ -15,6 +22,8 @@ def main():
     rows = spreadsheet.get_registrations()
     messages = {}
     playerlist = []
+    emaillist = []
+    emaillist.append( "Name, Email Address\n" )
     for idx, row in enumerate(rows):
          # email,sp_name,,home_planet_name,gov_name,gov_type,ML,GV,LS,BI
         email = row.custom["email"].text
@@ -32,12 +41,19 @@ def main():
         mil = row.custom["military"].text
         life = row.custom["lifesupport"].text
 
-        csv_row = "%s,%s,%s,%s,%s,%s,%s,%s,%s\n" % (email, species, home_planet, gov_name, gov_type, mil, grav, life, bio)
-        playerlist.append(csv_row)
+        playercsv_row = "%s,%s,%s,%s,%s,%s,%s,%s,%s\n" % (email, species, home_planet, gov_name, gov_type, mil, grav, life, bio)
+        playerlist.append(playercsv_row)
+        
+        emailcsv_row = "%s,%s\n" % (species, email)
+        emaillist.append(emailcsv_row)
 
-    csv = open('players.csv',"w")
-    csv.writelines(playerlist)
-    csv.close
+    player_csv = open('players.csv',"w")
+    player_csv.writelines(playerlist)
+    player_csv.close
+    
+    email_csv = open('players_email.csv',"w")
+    email_csv.writelines(emaillist)
+    email_csv.close
 
 if __name__ == "__main__":
     main()
