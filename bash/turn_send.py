8d9f3e5915410454256fa253b7cdb87ee3c26a4a
Casey Link <unnamedrambler@gmail.com>
2011-03-17 14:09:07 -0400

    add script to send out turn results

---
+++ bash/turn_send.py
@@ -0,0 +1,112 @@
+#!/usr/bin/python2
+"""
+    Usage: turn_send.py [-h] -c config.yml
+    
+    -h, --help      print this message
+    -c config.yml   use a particular config file
+    -t              perform a test run, do not send any email.
+
+    This script will read file fh_names and mail report files to the
+    appropriate players.  File deadline.msg will be inserted at the
+    head of each report file before mailing.
+
+"""
+import fhutils
+import os, sys, time
+import getopt
+from datetime import datetime
+from dateutil import zoneinfo
+from dateutil.relativedelta import *
+import dateutil
+
+deadline_msg ="""
+Orders for the next turn are due on %s at:
+%s
+
+Please format the subject line of your orders as in the following example:
+Subject: FH Orders, %s
+or
+Subject: FH Orders, %s wait
+
+See the game policies for information on 'wait'.
+
+I generally download orders within half an hour of the above deadline.  If they
+have not arrived by that time, or if the Subject line is not correctly
+formatted, then I will not be able to process them.
+"""
+
+message = """
+Your Far Horizons %s turn results are attached.
+
+%s
+
+As always, if there any problems, or you have a question, then shoot me an email.
+
+Best,
+Casey
+Gamemaster
+"""
+def main(argv):
+    config_file = None
+    test_flag = False
+    try:                                
+        opts, args = getopt.getopt(argv, "hc:t", ["help", "config=","test"])
+    except getopt.GetoptError:          
+        print __doc__                     
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-h", "--help"): 
+            print __doc__                     
+            sys.exit(0)
+        elif opt in ("-c", "--config"):
+            config_file = arg
+        elif opt in ("-t", "--test"):
+            test_flag = True
+
+    if config_file:
+        config = fhutils.GameConfig(config_file)
+    else:
+        config = fhutils.GameConfig()
+    game = config.gameslist[0] # for now we only support a single game
+    game_name = game['name']
+    game_stub = game['stub']
+    deadline_rule = game['deadline']
+    data_dir = game['datadir']
+    bin_dir = config.bindir
+    players = fhutils.Game().players
+    
+    if not os.path.isdir(data_dir):
+        print "Sorry data directory %s does not exist." % (data_dir)
+        sys.exit(2)
+        
+    if not os.path.isdir(bin_dir):
+        print "Sorry bin directory %s does not exist." % (bin_dir)
+        sys.exit(2)
+    
+    turn = fhutils.run(bin_dir, "TurnNumber").strip()
+    global message,deadline_msg
+    next_deadline = deadline_rule.after(datetime.now(dateutil.tz.tzutc()))
+    est = zoneinfo.gettz('America/New_York')
+    pst = zoneinfo.gettz('America/Los_Angeles')
+    day = next_deadline.strftime("%A %B %d")
+    time = next_deadline.strftime("%H:%M (%I:%M %p) %Z")
+    time += "\n= " + next_deadline.astimezone(est).strftime("%I:%M %p %Z")
+    time += "\n= " + next_deadline.astimezone(pst).strftime("%I:%M %p %Z")
+
+    deadline_msg = deadline_msg %(day, time,game_stub, game_stub)
+    message = message %(game_name, deadline_msg)
+    for player in players:
+        report = "%s/sp%s.rpt.t%s" %(data_dir, player['num'], turn)
+        subject = "FH Turn Results - SP %s turn %s" % (player['name'], turn)        
+        if not test_flag:
+            print "Mailing %s to %s (sp %s)" %(report, player['email'], player['name'])
+            config.send_mail(subject, player['email'], message, report)
+        else:
+            with open(report+".test", "w") as f:
+                f.write("To: %s\n" %( player['email']))
+                f.write("Subject: %s\n" %( subject))
+                f.write(message)
+                f.write("Attached: %s\n"  % (report))
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
