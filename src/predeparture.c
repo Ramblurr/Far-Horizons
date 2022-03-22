40d8e6c7113f8fb7f5b5b2ac3e90b4982f326ede
Michael Henderson <mdhender@mdhender.com>
2022-03-22 16:49:19 -0600

    added debug for send status

--- src/predeparture.c
+++ src/predeparture.c
@@ -349,8 +349,10 @@ int preDepartureSpecies(int spNo, int do_all_species, int first_pass) {
         log_string("\nPre-departure orders:\n");
     }
 
+    fprintf(stderr, "debug: preDepartureSpecies: SP%02d data_modified %s\n", species_index + 1, data_modified[species_index] ? "true" : "false");
     /* Handle predeparture orders for this species. */
     do_predeparture_orders();
+    fprintf(stderr, "debug: preDepartureSpecies: ..%02d data_modified %s\n", species_index + 1, data_modified[species_index] ? "true" : "false");
 
     data_modified[species_index] = TRUE;
 
