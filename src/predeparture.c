d2ccbfc6365883bcbb5ca8fc5173df572c1d3003
Michael Henderson <mdhender@mdhender.com>
2022-03-22 16:57:59 -0600

    changed species debug default number

--- src/predeparture.c
+++ src/predeparture.c
@@ -356,7 +356,7 @@ int preDepartureSpecies(int spNo, int do_all_species, int first_pass) {
     /* Handle predeparture orders for this species. */
     do_predeparture_orders();
     fprintf(stderr, "debug: preDepartureSpecies: ..%02d data_modified %s\n", species_index + 1, data_modified[species_index] ? "true" : "false");
-    fprintf(stderr, "debug: preDepartureSpecies: ..%02d data_modified %s\n", 16, data_modified[16 - 1] ? "true" : "false");
+    fprintf(stderr, "debug: preDepartureSpecies: ..%02d data_modified %s\n", 15, data_modified[15 - 1] ? "true" : "false");
 
     data_modified[species_index] = TRUE;
 
